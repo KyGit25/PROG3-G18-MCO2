6d368d6a4c306a9926dac180f207d14d754a9000
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 01:06:09 +0800

    Update Elephant.java

--- code/model/pieces/Elephant.java
+++ code/model/pieces/Elephant.java
@@ -4,27 +4,27 @@ import model.tiles.Tile;
 import model.tiles.HomeBase;
 
 public class Elephant extends Piece {
-    public Elephant(Tile pos) {
-        super(pos);
+    public Elephant(Tile pos, String owner) {
+        super(pos, owner);
     }
 
     @Override
-    public boolean canCapture(Piece piece) {
-        if (piece instanceof Rat) return false;
-        return this.getStrength() >= piece.getStrength();
+    public boolean canCapture(Piece opponent) {
+        // Elephant can't capture rat (special rule)
+        if (opponent instanceof Rat) return false;
+        return this.getStrength() >= opponent.getStrength();
     }
 
     @Override
     public boolean canMove(Tile destination) {
-
-        if (destination instanceof HomeBase && ((HomeBase) destination).getOwner().equals(this.getOwner())) {
-            return false;
-        }
-        return true;
+        // Standard orthogonal movement (1 square)
+        int rowDiff = Math.abs(destination.getRow() - pos.getRow());
+        int colDiff = Math.abs(destination.getCol() - pos.getCol());
+        return (rowDiff == 1 && colDiff == 0) || (rowDiff == 0 && colDiff == 1);
     }
 
     @Override
     public int getStrength() {
-        return 8;
+        return 8; // Strongest piece
     }
 }
