600bfc21781d87c9694fc2a4f5e8789c84fdc214
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-01 23:19:46 +0800

    Update HomeBase.java

--- code/model/tiles/HomeBase.java
+++ code/model/tiles/HomeBase.java
@@ -1,24 +1,8 @@
-/**
- * The HomeBase class represents a player's home base in the game.
- * It stores the position and owner of the home base.
- */
-public class HomeBase extends Tile {
-    
-    /**
-     * The owner of the home base ("Blue" or "Green").
-     */
-    private String owner;
+package model.tiles;
 
-    /**
-     * Constructs a HomeBase with the given row, column, and owner.
-     * 
-     * @param row   The row position of the home base.
-     * @param col   The column position of the home base.
-     * @param owner The owner of the home base ("Blue" or "Green").
-     * 
-     * Pre-condition: row and col must be within the board boundaries.
-     * Post-condition: A new HomeBase object is created with the specified attributes.
-     */
+public class HomeBase extends Tile {
+    private String owner; // "Blue" or "Green"
+
     public HomeBase(int row, int col, String owner) {
         super(row, col);
         this.owner = owner;
@@ -26,11 +10,11 @@ public class HomeBase extends Tile {
 
     @Override
     public void render() {
-        // TODO
+        // Rendering logic for home base tile
     }
 
-    @Override
-    protected String getOwner() {
-        return this.owner;
+    public String getOwner() 
+    {
+        return owner;
     }
 }
