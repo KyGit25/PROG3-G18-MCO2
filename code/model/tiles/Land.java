9644e36a4f1c3ce63916e13313be340dc558adaf
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 05:52:31 +0800

    Update Land.java

--- code/model/tiles/Land.java
+++ code/model/tiles/Land.java
@@ -2,6 +2,18 @@ package model.tiles;
 
 public class Land extends Tile {
 
+    /**
+ * Constructs a Land tile at a specific row and column.
+ *
+ * Pre-condition:
+ * - row and col are valid tile coordinates.
+ *
+ * Post-condition:
+ * - A Land tile is created at the given location.
+ *
+ * @param row The row index of the land tile.
+ * @param col The column index of the land tile.
+ */
     public Land(int row, int col) {
         super(row, col);
     }
