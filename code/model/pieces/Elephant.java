44731627f47d6942b3aa93d1a885496da2db50b9
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 02:31:43 +0800

    Update Elephant.java

--- code/model/pieces/Elephant.java
+++ code/model/pieces/Elephant.java
@@ -30,7 +30,12 @@ public class Elephant extends Piece {
     }
 
     @Override
-    public boolean canMove(Tile destination) {
+    public boolean canMove(Tile destination) 
+    {
+        if (destination instanceof HomeBase && ((HomeBase) destination).getOwner().equals(this.owner)) 
+        {
+            return false;
+        }
         if (destination instanceof Lake) return false;
         if (destination.isOccupied() && destination.getCurrPiece().getOwner().equals(this.owner)) return false;
 
