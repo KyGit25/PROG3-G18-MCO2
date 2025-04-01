9dbaea075633b14824960548e0c9cc7b505f09e1
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 05:52:52 +0800

    Update Lake.java

--- code/model/tiles/Lake.java
+++ code/model/tiles/Lake.java
@@ -2,6 +2,18 @@ package model.tiles;
 
 public class Lake extends Tile {
 
+    /**
+ * Constructs a Lake tile at a specific row and column.
+ *
+ * Pre-condition:
+ * - row and col are valid tile coordinates.
+ *
+ * Post-condition:
+ * - A Lake tile is created at the given location.
+ *
+ * @param row The row index of the lake tile.
+ * @param col The column index of the lake tile.
+ */
     public Lake(int row, int col) {
         super(row, col);
     }
