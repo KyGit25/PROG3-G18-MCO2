bb4e791359fba05c6d1049b95f5b4f35d3180f0e
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 02:11:52 +0800

    Update Trap.java

--- code/model/tiles/Trap.java
+++ code/model/tiles/Trap.java
@@ -1,19 +1,20 @@
 package model.tiles;
 
 public class Trap extends Tile {
-    private String owner; // "Blue" or "Green"
+    private String owner;
 
     public Trap(int row, int col, String owner) {
         super(row, col);
         this.owner = owner;
     }
 
-    @Override
-    public void render() {
-        // Rendering logic for trap tile
-    }
-
     public String getOwner() {
         return owner;
+    }
+
+    @Override
+    public void render() 
+    {
+        // Render logic customize in view
     }
 }
