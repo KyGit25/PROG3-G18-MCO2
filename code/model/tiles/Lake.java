e3c57aac063cfc5faf1ecda07a8564600ba0cc6f
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 02:11:19 +0800

    Update Lake.java

--- code/model/tiles/Lake.java
+++ code/model/tiles/Lake.java
@@ -1,14 +1,14 @@
 package model.tiles;
 
-public class Lake extends Tile 
-{
-    public Lake(int row, int col) 
-    {
+public class Lake extends Tile {
+
+    public Lake(int row, int col) {
         super(row, col);
     }
 
     @Override
-    public void render() {
-        // Rendering logic for lake tile
+    public void render() 
+    {
+        // Render logic customize in view
     }
 }
