65e378d10d396fe953dd78d62cce85aac42e6406
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 06:08:57 +0800

    Update Swimming.java

--- code/model/interfaces/Swimming.java
+++ code/model/interfaces/Swimming.java
@@ -2,6 +2,23 @@ package model.interfaces;
 
 import model.tiles.Tile;
 
-public interface Swimming {
+/**
+ * Interface for pieces that can swim in lake tiles.
+ */
+public interface Swimming 
+{
+    /**
+     * Determines if a piece can swim into a specified tile.
+     *
+     * Pre-condition:
+     * - The destination must be a tile adjacent to current position.
+     *
+     * Post-condition:
+     * - Returns the destination if it is a valid lake tile.
+     * - Returns null if the tile cannot be swum into.
+     *
+     * @param destination The target tile.
+     * @return The destination if swim is allowed; otherwise, null.
+     */
     Tile swim(Tile destination);
 }
