cc73e6f9b432049bca9a9b187aed3b1027afb0fb
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 05:53:55 +0800

    Update HomeBase.java

--- code/model/tiles/HomeBase.java
+++ code/model/tiles/HomeBase.java
@@ -3,11 +3,36 @@ package model.tiles;
 public class HomeBase extends Tile {
     private String owner;
 
+/**
+ * Constructs a HomeBase tile with owner designation.
+ *
+ * Pre-condition:
+ * - row and col are valid coordinates.
+ * - owner must be a valid team name (e.g., "Blue" or "Green").
+ *
+ * Post-condition:
+ * - A HomeBase tile is created and assigned to the given player.
+ *
+ * @param row The row index of the home base.
+ * @param col The column index of the home base.
+ * @param owner The owning player's name.
+ */
     public HomeBase(int row, int col, String owner) {
         super(row, col);
         this.owner = owner;
     }
 
+/**
+ * Gets the owner of this home base tile.
+ *
+ * Pre-condition:
+ * - None.
+ *
+ * Post-condition:
+ * - Returns the name of the player who owns this home base.
+ *
+ * @return The owner's name.
+ */
     public String getOwner() {
         return owner;
     }
