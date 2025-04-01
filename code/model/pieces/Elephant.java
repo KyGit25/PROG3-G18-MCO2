24fcce9acccafe4070031fa7d074f7af5e5b92e3
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-01 23:21:46 +0800

    Update Elephant.java

--- code/model/pieces/Elephant.java
+++ code/model/pieces/Elephant.java
@@ -1,3 +1,29 @@
-public class Elephant extends Piece{
+package model.pieces;
 
+import model.tiles.Tile;
+
+public class Elephant extends Piece {
+    public Elephant(Tile pos) {
+        super(pos);
+    }
+
+    @Override
+    public boolean canCapture(Piece piece) {
+        if (piece instanceof Rat) return false;
+        return this.getStrength() >= piece.getStrength();
+    }
+
+    @Override
+    public boolean canMove(Tile destination) {
+
+        if (destination instanceof HomeBase && ((HomeBase) destination).getOwner().equals(this.getOwner())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int getStrength() {
+        return 8;
+    }
 }
