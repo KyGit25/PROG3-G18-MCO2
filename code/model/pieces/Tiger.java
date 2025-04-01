49d019532a2300bd1ccbd74880212e941a1b3439
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 01:09:43 +0800

    Update Tiger.java

--- code/model/pieces/Tiger.java
+++ code/model/pieces/Tiger.java
@@ -6,8 +6,8 @@ import model.tiles.Tile;
 import model.tiles.HomeBase;
 
 public class Tiger extends Piece implements Leaping {
-    public Tiger(Tile pos) {
-        super(pos);
+    public Tiger(Tile pos, String owner) {
+        super(pos, owner);
     }
 
     @Override
