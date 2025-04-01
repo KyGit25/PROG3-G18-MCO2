e88db12c4977aaf86e7290c7519b130dd75fcb67
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-02 02:14:51 +0800

    Update Dog.java

--- code/model/pieces/Dog.java
+++ code/model/pieces/Dog.java
@@ -1,30 +1,38 @@
 package model.pieces;
 
-import model.tiles.Tile;
-import model.tiles.HomeBase;
+import model.tiles.*;
 
 public class Dog extends Piece {
-    public Dog(Tile pos, String owner) {
-        super(pos, owner);
-    }
 
-    @Override
-    public boolean canCapture(Piece piece) {
-        return this.getStrength() >= piece.getStrength();
-    }
-
-    @Override
-    public boolean canMove(Tile destination) {
-        // Standard movement rules
-        // Cannot move into own home base
-        if (destination instanceof HomeBase && ((HomeBase) destination).getOwner().equals(this.getOwner())) {
-            return false;
-        }
-        return true;
+    public Dog(Tile pos) {
+        super(pos);
     }
 
     @Override
     public int getStrength() {
         return 3;
+    }
+
+    @Override
+    public boolean canCapture(Piece target) {
+        if (target == null || target.getOwner().equals(this.owner)) return false;
+
+        if (target.getPosition() instanceof Trap &&
+            !((Trap) target.getPosition()).getOwner().equals(this.owner)) {
+            return true;
+        }
+
+        return this.getStrength() >= target.getStrength();
+    }
+
+    @Override
+    public boolean canMove(Tile destination) {
+        if (destination instanceof Lake) return false;
+        if (destination.isOccupied() && destination.getCurrPiece().getOwner().equals(this.owner)) return false;
+
+        int dx = Math.abs(pos.getRow() - destination.getRow());
+        int dy = Math.abs(pos.getCol() - destination.getCol());
+
+        return (dx + dy == 1);
     }
 }
