fa2e9fb343d7429f3e07d1331a88399b85f9e92d
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-04-01 23:20:21 +0800

    Update Land.java

--- code/model/tiles/Land.java
+++ code/model/tiles/Land.java
@@ -1,20 +1,13 @@
+package model.tiles;
+
 public class Land extends Tile {
-    
     public Land(int row, int col) {
         super(row, col);
     }
-    
+
     @Override
-    public void render() {
-       if(this.currentPiece != null){
-        // TODO
-       }else{
-        System.out.print(" ");
-       }
-    }
-    
-    @Override
-    protected String getOwner() {
-        return null; // Land tiles don't have an owner
+    public void render() 
+    {
+        // Rendering logic for land tile
     }
 }
